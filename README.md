ab2ebea43dc559fad4bd8d63d57bbed732481dca
Juan Simonetti <sjuanandres0@gmail.com>
2024-01-25 10:09:09 +0100

    Basic page with navbar and footer

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 [Link](https://capataz-eight.vercel.app/)
 
 #### Help
-> py -m venv env
+> py -m venv env \
 > env\Scripts\activate
-
-
+- SimpleIcons: https://simpleicons.org/
+- HeroIcons: https://heroicons.com/
+- FontAwesome: https://fontawesome.com/
 
--- api/index.py
+++ api/index.py
@@ -1,9 +1,15 @@
-from flask import Flask
+import os
+import json
+from flask import Flask, render_template, session, redirect, request, abort, flash, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    return render_template('layout.html', title='Home')
+
+@app.route('/hello_world')
+def hello_world():
     return 'Hello, World!'
 
 @app.route('/about')
