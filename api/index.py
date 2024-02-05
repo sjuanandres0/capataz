5c7fc3581c8895089b0cd1b2f4b23744e0dd834e
Juan Simonetti <sjuanandres0@gmail.com>
2024-02-05 20:51:43 +0100

    Login implemented with hardcoded pw in environment variables

--- api/index.py
+++ api/index.py
@@ -3,74 +3,69 @@ import sys
 import json
 from datetime import datetime
 from flask import Flask, render_template, session, redirect, request, abort, flash, jsonify
+from flask_session import Session
 from functools import wraps
 
 # print(sys.path)
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-from db import client
 
+from db import client
 
 app = Flask(__name__)
 app.secret_key = "TukiTukiSecretKey"
-password_accepted = ['TukiTuki']
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
-# Session(app)
+password_accepted = os.environ.get("password_accepted")
+if password_accepted is None:
+    from credentials import password_accepted
+password_accepted = [password_accepted]
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 def login_is_required(view_func):
     @wraps(view_func)
     def decorated_func(*args, **kwargs):
-        if 'password' not in session or session['password'] not in password_accepted:
+        if not session.get('name'):
             return abort(401)  # Authorization required
         else:
             return view_func(*args, **kwargs)
     return decorated_func
 
 
-@app.route('/test')
-def test():
-    collection = client.dummyDatabase.dummyCollection
-    result = collection.find_one()
-    print(result)
-    return json.dumps(result, default=str)
+# @app.route('/test')
+# def test():
+#     collection = client.dummyDatabase.dummyCollection
+#     result = collection.find_one()
+#     print(result)
+#     return json.dumps(result, default=str)
 
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/home', methods=['GET', 'POST'])
 def home():
-    if 'password' in session:
-        if session['password'] in password_accepted:
-            return render_template('home.html')
-    else:
+    if not session.get('name'):
         return redirect('/login')
+    return render_template('home.html')
 
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
-    if 'password' in session:
-        if session['password'] in password_accepted:
-            return redirect('/')
-
+    if session.get('name'):
+        return redirect('/')
     if request.method == 'POST':
-        input_password = request.form.get('input_password')
-        print(f'input_password: {input_password}')
+        input_password = request.form.get('password')
         if input_password in password_accepted:
-            session.permanent = False
-            session['password'] = input_password
             flash(['Successfully logged in!', 'success'])
+            session['name'] = request.form.get('name')
             return redirect('/')
         else:
-            flash(['Contrasena incorrecta. Intente nuevamente.', 'danger'])
-            return render_template('login.html', title='Login')
-    else:
-        return render_template('login.html', title='Login')
+            flash(['Incorrect password!', 'danger'])
+    return render_template('login.html', title='Login')
 
 
 @app.route('/logout')
 def logout():
-    if 'password' in session:
-        session.clear()
+    session.clear()
     flash(['Successfully logged out!', 'success'])
     return redirect('/')
 
@@ -122,50 +117,50 @@ def about():
 #     return render_template('cp.html', title='Cartas de Porte', params=params)
 
 
-@login_is_required
 @app.route('/wip')
+@login_is_required
 def wip():
     return render_template('wip.html', title='WIP')
 
 
-@login_is_required
 @app.route('/contracts')
+@login_is_required
 def contracts():
     return render_template('wip.html', title='Contratos')
 
 
-@login_is_required
 @app.route('/lpg')
+@login_is_required
 def lpg():
     return render_template('wip.html', title='Liquidaciones')
 
 
-@login_is_required
 @app.route('/dash')
+@login_is_required
 def dash():
     return render_template('wip.html', title='Dashboard')
 
 
-@login_is_required
 @app.route('/settings')
+@login_is_required
 def settings():
     return render_template('wip.html', title='Configuracion')
 
 
-@login_is_required
 @app.route('/agricultura')
+@login_is_required
 def agricultura():
     return render_template('agricultura.html', title='Agricultura')
 
 
-@login_is_required
 @app.route('/configuracion')
+@login_is_required
 def configuracion():
     return render_template('configuracion.html', title='Configuracion')
 
 
-@login_is_required
 @app.route('/configuracion/establecimiento', methods=['GET', 'POST'])
+@login_is_required
 def establecimiento():
     if request.method == 'POST':
         action = request.form['action']
