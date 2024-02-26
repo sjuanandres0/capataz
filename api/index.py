09dda3d2352a1d33a4828da6e5cae68333a984aa
Juan Simonetti <sjuanandres0@gmail.com>
2024-02-26 21:07:02 +0100

    Extract CTG from PDF

--- api/index.py
+++ api/index.py
@@ -494,18 +494,43 @@ def reportes():
     params = {'cp': cp}
     return render_template('report_apex.html', title='Reportes', params=params)
 
-@app.route('/cp_pdf_reader')
+
+from werkzeug.utils import secure_filename
+from io import BytesIO
+
+ALLOWED_EXTENSIONS = {'pdf'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/cp_pdf_reader',methods=['GET','POST'])
 @login_is_required
 def cp_pdf_reader():
-    text = extract_text("cp_dummy_test_1.pdf")
-    pattern = r'\nCTG: (\d{11})*'
-    match = re.search(pattern, text)
-    if match:
-        ctg = match.group(1)
-        print('ctg:',ctg)
-        return ctg
-    else:
-        return 'fail'
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash(['No file part','Success'])
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            print(f"File name: {secure_filename(file.filename)}")
+            print(f"File content type: {file.content_type}")
+            if file:
+                pdf_data = file.read()
+                text = extract_text(BytesIO(pdf_data))
+            pattern = r'\nCTG: (\d{11})*'
+            match = re.search(pattern, text)
+            if match:
+                ctg = match.group(1)
+                print('ctg:',ctg)
+                # return ctg
+                return jsonify({'ctg': ctg})
+            else:
+                return 'fail'
+    return render_template('upload_file_dummy.html')
 
 
 if __name__ == '__main__':
